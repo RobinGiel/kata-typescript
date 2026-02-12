13aff979bb172d446fb8cef13cfc211064dc542a
RobinGiel <robingiel@gmail.com>
2026-02-12 12:09:41 +0100

    refactor: simplify updateQuality method and enhance quality increase logic

--- TypeScript/src/updaters/backstage-pass-updater.ts
+++ TypeScript/src/updaters/backstage-pass-updater.ts
@@ -3,21 +3,23 @@ import type { Item } from "@/item";
 import { StandardItemUpdater } from "@/updaters/standard-item-updater";
 
 export class BackstagePassUpdater extends StandardItemUpdater {
-	protected updateQuality(item: Item): void {
-		if (item.sellIn < 0) {
-			// After the conference, quality drops to 0
-			item.quality = 0;
-		} else {
-			// Quality increases as conference approaches
-			let increaseAmount = 1;
+    protected updateQuality(item: Item): void {
+        if (item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
 
-			if (item.sellIn <= SellInThresholds.BACKSTAGE_HIGH) {
-				increaseAmount = 3;
-			} else if (item.sellIn <= SellInThresholds.BACKSTAGE_MEDIUM) {
-				increaseAmount = 2;
-			}
+        const increaseAmount = this.getQualityIncreaseAmount(item.sellIn);
+        this.increaseQuality(item, increaseAmount);
+    }
 
-			this.increaseQuality(item, increaseAmount);
-		}
-	}
+    private getQualityIncreaseAmount(sellIn: number): number {
+        if (sellIn <= SellInThresholds.BACKSTAGE_HIGH) {
+            return 3;
+        }
+        if (sellIn <= SellInThresholds.BACKSTAGE_MEDIUM) {
+            return 2;
+        }
+        return 1;
+    }
 }
